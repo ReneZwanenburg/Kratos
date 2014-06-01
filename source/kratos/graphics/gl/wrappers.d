aed80d1afc440f45698213159e1e9eb38a18da42
Rene Zwanenburg <renezwanenburg@gmail.com>
2014-06-01 17:12:27 +0200

    Add module for D-like wrappers around GL objects

--- source/kratos/graphics/gl/wrappers.d
+++ source/kratos/graphics/gl/wrappers.d
@@ -1,2 +1,26 @@
 ﻿module kratos.graphics.gl.wrappers;
 
+import kratos.graphics.gl.gl;
+
+struct VAO
+{
+	GLuint vertexArrayID;
+	alias vertexArrayID this;
+	
+	static opCall()
+	{
+		VAO vao;
+		gl.GenVertexArrays(1, &vao.vertexArrayID);
+		return vao;
+	}
+
+	~this()
+	{
+		gl.DeleteVertexArrays(1, &vertexArrayID);
+	}
+}
+
+void bind(VAO vao)
+{
+	gl.BindVertexArray(vao);
+}
