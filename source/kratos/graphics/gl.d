d89ec324641aaff30a7efe1c7b961bcbe0ee3f30
Rene Zwanenburg <renezwanenburg@gmail.com>
2014-07-18 17:00:50 +0200

    Cleanup GL wrapper

--- source/kratos/graphics/gl.d
+++ source/kratos/graphics/gl.d
@@ -15,130 +15,92 @@ import std.conv : text;
 /// Thin OpenGL wrapper. Performs glGetError() error checking in debug mode.
 final abstract class gl
 {
-	static auto opDispatch(string functionName, Args...)(Args args)
+	static auto opDispatch(string functionName, Args...)(Args args) nothrow
 	{
-		enum fullFunctionName = "gl"~functionName;
-		mixin("alias func = " ~ fullFunctionName ~ ";");
+		//alias func = mixin("gl"~functionName); // Doesn't work, probably DMD bug
+		mixin("alias func = gl" ~ functionName ~ ";");
 
-		import std.traits : ReturnType;
-		static if(!is(ReturnType!func == void))
+
+		debug scope(success)
 		{
-			auto retVal = func(args);
-		}
-		else
-		{
-			func(args);
+			import std.exception : assumeWontThrow;
+			assumeWontThrow(checkGLError!func(args));
 		}
 
-		debug
-		{
-			if(auto errorCode = glGetError())
-			{
-				import std.stdio : writefln;
-				import std.traits : ParameterTypeTuple;
-
-				writefln("GL Error %s (%s)", errorCode, glEnumString.get(errorCode, "Unknown error code"));
-
-				string[] stringifiedArgs;
-				foreach(i, T; ParameterTypeTuple!func)
-				{
-					stringifiedArgs ~= args[i].text;
-
-					static if(is(T == GLenum))
-					{
-						if(auto enumNames = args[i] in glEnumString)
-						{
-							stringifiedArgs ~= "(" ~ glEnumString[args[i]] ~ ")";
-						}
-					}
-				}
-				writefln("While calling %s(%s);", fullFunctionName, stringifiedArgs.joiner(", "));
-
-				assert(false);
-			}
-		}
-
-		static if(!is(ReturnType!func == void))
-		{
-			return retVal;
-		}
+		return func(args);
 	}
 }
 
+private void checkGLError(alias func, Args...)(Args args)
+{
+	if(auto errorCode = glGetError())
+	{
+		import std.stdio : writefln;
+		import std.traits : ParameterTypeTuple;
+		
+		writefln("GL Error %s (%s)", errorCode, glEnumString.get(errorCode, "Unknown error code"));
+		
+		string[] stringifiedArgs;
+		foreach(i, T; ParameterTypeTuple!func)
+		{
+			stringifiedArgs ~= args[i].text;
+			
+			static if(is(T == GLenum))
+			{
+				if(auto enumNames = args[i] in glEnumString)
+				{
+					stringifiedArgs[$-1] ~= "<" ~ glEnumString[args[i]] ~ ">";
+				}
+			}
+		}
+		writefln("While calling %s(%s);", func.stringof, stringifiedArgs.joiner(", "));
+		assert(false);
+	}
+}
+
+private template GLTypeBinding(GLenum _glType, T)
+{
+	enum glType = _glType;
+	alias nativeType = T;
+}
+
+import std.typetuple;
+alias GLTypes = TypeTuple!(
+	GLTypeBinding!(GL_FLOAT, float),
+	GLTypeBinding!(GL_FLOAT_VEC2, vec2),
+	GLTypeBinding!(GL_FLOAT_VEC3, vec3),
+	GLTypeBinding!(GL_FLOAT_VEC4, vec4),
+
+	GLTypeBinding!(GL_INT, int),
+	GLTypeBinding!(GL_INT_VEC2, vec2i),
+	GLTypeBinding!(GL_INT_VEC3, vec3i),
+	GLTypeBinding!(GL_INT_VEC4, vec4i),
+
+	GLTypeBinding!(GL_BOOL, bool),
+
+	GLTypeBinding!(GL_FLOAT_MAT2, mat2),
+	GLTypeBinding!(GL_FLOAT_MAT3, mat3),
+	GLTypeBinding!(GL_FLOAT_MAT4, mat4)
+);
+
 template GLType(T)
 {
-	static if(is(T == float))
-	{
-		enum GLType = GL_FLOAT;
-	}
-	else static if(is(T == vec2))
-	{
-		enum GLType = GL_FLOAT_VEC2;
-	}
-	else static if(is(T == vec3))
-	{
-		enum GLType = GL_FLOAT_VEC3;
-	}
-	else static if(is(T == vec4))
-	{
-		enum GLType = GL_FLOAT_VEC4;
-	}
-	else static if(is(T == int))
-	{
-		enum GLType = GL_INT;
-	}
-	else static if(is(T == vec2i))
-	{
-		enum GLType = GL_INT_VEC2;
-	}
-	else static if(is(T == vec3i))
-	{
-		enum GLType = GL_INT_VEC3;
-	}
-	else static if(is(T == vec4i))
-	{
-		enum GLType = GL_INT_VEC4;
-	}
-	else static if(is(T == bool))
-	{
-		enum GLType = GL_BOOL;
-	}
-	else static if(is(T == mat2))
-	{
-		enum GLType = GL_FLOAT_MAT2;
-	}
-	else static if(is(T == mat3))
-	{
-		enum GLType = GL_FLOAT_MAT3;
-	}
-	else static if(is(T == mat4))
-	{
-		enum GLType = GL_FLOAT_MAT4;
-	}
-	
-	else static assert(false, "Not a valid OpenGL Type or Type not implemented");
+	enum GLType = {
+		foreach(B; GLTypes)
+		{
+			static if(is(B.nativeType == T)) return B.glType;
+		}
+		assert(false, "Not a valid OpenGL Type or Type not implemented");
+	}();
 }
 
 immutable GLsizei[GLenum] GLTypeSize;
 shared static this()
 {
-	GLTypeSize = [
-		GL_FLOAT		: float.sizeof,
-		GL_FLOAT_VEC2	: vec2.sizeof,
-		GL_FLOAT_VEC3	: vec3.sizeof,
-		GL_FLOAT_VEC4	: vec4.sizeof,
-		
-		GL_INT			: int.sizeof,
-		GL_INT_VEC2		: vec2i.sizeof,
-		GL_INT_VEC3		: vec3i.sizeof,
-		GL_INT_VEC4		: vec4i.sizeof,
-		
-		GL_BOOL			: bool.sizeof,
-		
-		GL_FLOAT_MAT2	: mat2.sizeof,
-		GL_FLOAT_MAT3	: mat3.sizeof,
-		GL_FLOAT_MAT4	: mat4.sizeof
-	];
+	foreach(B; GLTypes)
+	{
+		GLTypeSize[B.glType] = B.nativeType.sizeof;
+	}
 }
 
 debug
