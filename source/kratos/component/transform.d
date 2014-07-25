089a2115809b9f423f5226e333fc6a3fce5c5e1d
Rene Zwanenburg <renezwanenburg@gmail.com>
2014-07-25 12:20:59 +0200

    Flesh Transform out a bit

--- source/kratos/component/transform.d
+++ source/kratos/component/transform.d
@@ -6,5 +6,9 @@ import gl3n.linalg;
 
 final class Transform : Component
 {
-
+	private Transform	_parent		= null;
+	private vec3		_position	= vec3(0);
+	private quat		_rotation	= quat.identity;
+	//TODO: Decide if non-uniform scaling should be supported
+	private float		_scale		= 1;
 }
