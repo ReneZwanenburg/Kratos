f424943640f4ecc58a7cbcb9b6341b955601c276
Rene Zwanenburg <renezwanenburg@gmail.com>
2014-07-27 01:17:30 +0200

    Start on render states

---
+++ source/kratos/graphics/renderstate.d
@@ -0,0 +1,51 @@
+﻿module kratos.graphics.renderstate;
+
+import kratos.graphics.gl;
+
+
+
+struct RenderState
+{
+	DepthTest	depthTest;
+
+	void apply()
+	{
+		foreach(state; this.tupleof)
+		{
+			if(state !is typeof(state).current)
+			{
+				state.apply();
+			}
+		}
+	}
+}
+
+enum DepthFunc : GLenum
+{
+	Never			= GL_NEVER,
+	Less			= GL_LESS,
+	Equal			= GL_EQUAL,
+	LessOrEqual		= GL_LEQUAL,
+	Greater			= GL_GREATER,
+	NotEqual		= GL_NOTEQUAL,
+	GreaterOrEqual	= GL_GEQUAL,
+	Always			= GL_ALWAYS
+}
+
+struct DepthTest
+{
+	private static current = DepthTest(DepthFunc.Less, false, true);
+
+	DepthFunc	func	= DepthFunc.LessOrEqual;
+	bool		read	= true;
+	bool		write	= true;
+
+	void apply()
+	{
+		if(current.func != func)	gl.DepthFunc(func);
+		if(current.read != read)	gl.setEnabled(GL_DEPTH_TEST, read);
+		if(current.write != write)	gl.DepthMask(write);
+
+		current = this;
+	}
+}
