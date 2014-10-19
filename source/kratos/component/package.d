c34c71b5e49d035b83125dda5d9b5b58c403aec6
Rene Zwanenburg <renezwanenburg@gmail.com>
2014-10-19 20:36:07 +0200

    Make life easier, use package.d ;)

---
+++ source/kratos/component/package.d
@@ -0,0 +1,6 @@
+﻿module kratos.component;
+
+public import kratos.component.transform;
+public import kratos.component.meshrenderer;
+public import kratos.component.simplemovement;
+public import kratos.component.camera;
---
+++ source/kratos/package.d
@@ -0,0 +1,13 @@
+﻿module kratos;
+
+public import kratos.entity;
+public import kratos.time;
+public import kratos.input;
+
+public import kgl3n.vector;
+public import kgl3n.matrix;
+public import kgl3n.quaternion;
+public import kgl3n.interpolate;
+public import kgl3n.math;
+
+public import std.experimental.logger;
