5986b77e0f95f3b8c24e36571b4ff17cd28b888a
Rene Zwanenburg <renezwanenburg@gmail.com>
2014-10-19 19:58:09 +0200

    Make Camera easiliy serializable

--- source/kratos/component/camera.d
+++ source/kratos/component/camera.d
@@ -2,22 +2,31 @@
 
 import kratos.entity;
 import kratos.component.transform;
-
 import kgl3n.matrix;
+import vibe.data.json;
 
-private alias registration = RegisterComponent!Camera;
+mixin RegisterComponent!Camera;
+
+struct StandardProjection
+{
+	@optional:
+	float aspectRatio = 0;
+	float fov = 60;
+	float nearPlane = .1f;
+	float farPlane = 2000;
+}
 
 final class Camera : Component
 {
-	@optional:
 	private @dependency Transform transform;
 
-	public mat4 projectionMatrix;
+	private mat4				_projectionMatrix;
+	private StandardProjection	standardProjection;
+	private bool				customProjection = false;
 
 	this()
 	{
-		import kratos.window;
-		projectionMatrix = perspectiveProjection(Window.activeProperties.width, Window.activeProperties.height, 60, .1f, 2000);
+		projectionMatrix = StandardProjection();
 
 		if(current is null)
 		{
@@ -38,17 +47,77 @@ final class Camera : Component
 		_current = this;
 	}
 
-	public @property mat4 viewProjectionMatrix() const
+	@property
 	{
-		return projectionMatrix * transform.worldMatrixInv;
+		mat4 viewProjectionMatrix() const
+		{
+			return projectionMatrix * transform.worldMatrixInv;
+		}
+		
+		mat4 viewMatrix() const
+		{
+			return transform.worldMatrixInv;
+		}
+
+		mat4 projectionMatrix() const
+		{
+			return _projectionMatrix;
+		}
+
+		void projectionMatrix(mat4 matrix)
+		{
+			_projectionMatrix = matrix;
+			customProjection = true;
+		}
+
+		void projectionMatrix(StandardProjection projection)
+		{
+			this.standardProjection = projection;
+
+			import kratos.window;
+			auto aspect = projection.aspectRatio <= 0 ? 
+				(Window.activeProperties.width / cast(float)Window.activeProperties.height) : 
+				projection.aspectRatio;
+
+			_projectionMatrix = perspectiveProjection(aspect, 1, projection.fov, projection.nearPlane, projection.farPlane);
+			customProjection = false;
+		}
 	}
 
-	public @property mat4 viewMatrix() const
+
+	Json toRepresentation()
 	{
-		return transform.worldMatrixInv;
+		auto json = Json.emptyObject;
+		json["customProjection"] = customProjection;
+		if(customProjection)
+		{
+			json["projection"] = _projectionMatrix.serializeToJson;
+		}
+		else
+		{
+			json["projection"] = standardProjection.serializeToJson;
+		}
+
+		return json;
 	}
 
-	static{
+	static Camera fromRepresentation(Json json)
+	{
+		auto camera = new Camera();
+
+		if(json["customProjection"].get!bool)
+		{
+			camera.projectionMatrix = json["projection"].deserializeJson!mat4;
+		}
+		else
+		{
+			camera.projectionMatrix = json["projection"].deserializeJson!StandardProjection;
+		}
+
+		return camera;
+	}
+
+	static {
 		private Camera _current;
 
 		public @property Camera current()
