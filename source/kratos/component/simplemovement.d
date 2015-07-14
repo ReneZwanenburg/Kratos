fbfed55879c38c31f9f6f3ddd1b7eee2f7e12164
ReneZwanenburg <renezwanenburg@gmail.com>
2015-07-15 00:00:16 +0200

    Improve SimpleMovement, fix direction when rotated

--- source/kratos/component/simplemovement.d
+++ source/kratos/component/simplemovement.d
@@ -3,6 +3,7 @@
 import kratos.ecs;
 import kratos.input;
 import kratos.component.transform;
+import kratos.time : Time;
 import kgl3n.vector;
 import kgl3n.quaternion;
 import kgl3n.math;
@@ -24,15 +25,33 @@ final class SimpleMovement : Component
 
 		transform.rotation = quat.eulerRotation(ypr);
 
-		auto forward = transform.worldMatrix[2].xyz;
+		auto forward = (transform.rotation * vec3(0, 0, -1)) * speed * Time.delta;
+		auto right = (transform.rotation * vec3(1, 0, 0)) * speed * Time.delta;
+		auto up = (transform.rotation * vec3(0, 1, 0)) * speed * Time.delta;
 
-		if(keyboard["Up"].pressed)
+		if(keyboard["W"].pressed)
 		{
-			transform.position = transform.position + forward * speed;
+			transform.position = transform.position + forward;
 		}
-		if(keyboard["Down"].pressed)
+		if(keyboard["S"].pressed)
 		{
-			transform.position = transform.position - forward * speed;
+			transform.position = transform.position - forward;
+		}
+		if(keyboard["A"].pressed)
+		{
+			transform.position = transform.position - right;
+		}
+		if(keyboard["D"].pressed)
+		{
+			transform.position = transform.position + right;
+		}
+		if(keyboard["Q"].pressed)
+		{
+			transform.position = transform.position + up;
+		}
+		if(keyboard["Z"].pressed)
+		{
+			transform.position = transform.position - up;
 		}
 	}
 }
