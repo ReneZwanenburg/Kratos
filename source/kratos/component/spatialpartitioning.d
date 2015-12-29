814c2859fade7407e4852c721d92254ddaabb7e2
ReneZwanenburg <renezwanenburg@gmail.com>
2015-12-29 13:48:35 +0100

    Add simple frustum culling to SpatialPartitioning

--- source/kratos/component/spatialpartitioning.d
+++ source/kratos/component/spatialpartitioning.d
@@ -1,14 +1,26 @@
 ﻿module kratos.component.spatialpartitioning;
 
 import kratos.ecs.scene : SceneComponent;
+import kgl3n.frustum : Frustum;
+import std.algorithm.iteration : filter;
 
 public final class SpatialPartitioning(ComponentType) : SceneComponent
 {
+	enum hasBounds = is(typeof(ComponentType.init.worldSpaceBound));
+	
 	private ComponentType[] _components;
 	
 	auto all()
 	{
 		return _components[];
+	}
+	
+	static if(hasBounds)
+	{
+		auto intersecting(Frustum frustum)
+		{
+			return all.filter!(a => frustum.intersects(a.worldSpaceBound));
+		}
 	}
 	
 	void register(ComponentType component)
