9918caf4d1dc0f8544d24b3665df7695482f9e98
ReneZwanenburg <renezwanenburg@gmail.com>
2016-02-02 13:43:25 +0100

    Refactoring

--- source/kratos/resource/loader/internal.d
+++ source/kratos/resource/loader/internal.d
@@ -1,20 +1,9 @@
 ﻿module kratos.resource.loader.internal;
 
+public import kratos.resource.loader.jsonloader;
 public import kratos.resource.filesystem;
 
-import vibe.data.json : Json, parseJsonString;
-
-package:
-
-/// Uncached JSON loader. The idea is that the resource reprsented by
-/// the JSON document will be cached, so there's no need to cache the
-/// document itself
-Json loadJson(ResourceIdentifier name)
-{
-	return activeFileSystem.get!char(name).parseJsonString();
-}
-
-@property auto lowerCaseExtension(string path)
+package @property auto lowerCaseExtension(string path)
 {
 	import std.path : extension;
 	import std.string : toLower;
